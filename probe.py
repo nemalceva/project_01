868cb18f860ca55dcb680c20a47961024ed5383e
nemalceva <o.nemalceva@mail.ru>
2023-06-06 15:25:39 +0500

    Task 1 is done

--- probe.py
+++ probe.py
@@ -1,1 +1,3 @@
-print(2+2)
+# Задача 1.1
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(my_favorite_songs[0:14], my_favorite_songs[-13:], my_favorite_songs[16:30], my_favorite_songs[-26:-15], sep=" * ")
