c1eb295782d61ef61a32bfb0befb6e79c4e2054e
nemalceva <o.nemalceva@mail.ru>
2023-06-30 09:51:50 +0500

    deleted classwork (branch homework)

--- Classwork/Test1/get_path.py
+++
@@ -1,9 +0,0 @@
-import os
-def get_paths(path="."):
-    for name in os.listdir(path):
-        abs_path = os.path.abspath(os.path.join(path,name))
-        yield abs_path
-        if os.path.isdir(abs_path) is True:
-            yield from get_paths(abs_path)
-for i in get_paths('Test2'):
-    print(i)           
